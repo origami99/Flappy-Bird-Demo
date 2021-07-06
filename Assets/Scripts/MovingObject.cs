ef6c67a8103133434bd733eeb02e6a149d32aae8
Niki <origamista99@gmail.com>
2021-07-06 19:20:34 +0300

    07. Moving Object

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _xBound;
+
+    private void Update()
+    {
+        this.transform.position -= Vector3.right * _speed * Time.deltaTime;
+
+        if (this.transform.position.x < _xBound)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
